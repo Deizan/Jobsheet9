59cdf6b45e75784b8e9261aced4f69a58764a2b8
Deizan <deizanfakhri7@gmail.com>
2025-11-12 16:17:39 +0700

    percobaan ketiga

--- ArrayRataNilai16.java
+++ ArrayRataNilai16.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class ArrayRataNilai16 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner (System.in); 
 
         int[] nilaiMhs = new int[10];
         double total = 0; 
