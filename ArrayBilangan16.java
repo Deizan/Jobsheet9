6072fa7f95dca0490cc1762cfd1f2c5ac51f140f
Deizan <deizanfakhri7@gmail.com>
2025-11-12 12:42:33 +0700

    percobaan pertama

--- ArrayBilangan16.java
+++ ArrayBilangan16.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ArrayBilangan16 { 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
         int[] bil = new int[4];
         bil[0] = 5;
